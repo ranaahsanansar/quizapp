4225938666fcd32cca7f31024d1f22a3feadf00f
Rana Ahsan Ansar <asn.cs21@gmail.com>
2022-04-17 03:22:10 -0700

    Add Seter and getters in Mcqs Class

--- src/quizappone/Mcqs.java
+++ src/quizappone/Mcqs.java
@@ -32,5 +32,56 @@ public class Mcqs {
     
     }
     
+    public int getId(){
+    return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+//    -------------------------------
+
+    public String getQuestion(){
+        return this.Question_s;
+    }public void setQuestion(String Question){
+        
+        this.Question_s = Question;
+    }
+//    ----------------------------------
+    public String getOpt1(){
+        return this.opt1;
+    }
+    public void setOpt1(String opt1){
+        this.opt1 = opt1;
+    }
+    
+//    ----------------------------------------
+    public String getOpt2(){
+        return this.opt2;
+    }
+    public void setOpt2(String opt2){
+        this.opt2 = opt2;
+    }
+//    -----------------------------------------------
+    public String getOpt3(){
+        return this.opt3;
+    }
+    public void setOpt3(String opt3){
+        this.opt3 = opt3;
+    }
+//    ----------------------------------------------
+    public String getOpt4(){
+        return this.opt4;
+    }
+    public void setOpt4(String opt4){
+        this.opt4 = opt4;
+    }
+//    --------------------------------------------
+    public String getCorrect(){
+        return this.correct;
+    }
+    public void setCorrect(String corr){
+        this.correct = corr;
+    }
+    
     
 }
