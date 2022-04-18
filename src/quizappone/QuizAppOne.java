2521f25aff0511d6ac279bf8ab0390cc8b44adca
Rana Ahsan Ansar <asn.cs21@gmail.com>
2022-04-17 23:30:04 -0700

    Compelete the Attemp_Question Function and modify Constructor of Student Class

--- src/quizappone/QuizAppOne.java
+++ src/quizappone/QuizAppOne.java
@@ -4,6 +4,7 @@
  */
 package quizappone;
 import java.sql.*;
+import java.util.Scanner;
 /**
  *
  * @author Rana Ahsan Ansar
@@ -13,12 +14,18 @@ public class QuizAppOne {
     /**
      * @param args the command line arguments
      */
+    
+    static Scanner input = new Scanner(System.in);
+    
     public static void main(String[] args) {
         // TODO code application logic here
         Teacher T = new Teacher("Captain" , 22);
 //        T.insert();
 //        T.update(1);
 //        System.out.println("Mcq UpdatedMian");
+        Student student = new Student("Ahsan" , 22 , 99);
+        student.attempt_Q();
+
     }
     
 }
--- src/quizappone/Student.java
+++ src/quizappone/Student.java
@@ -7,54 +7,107 @@ package quizappone;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
  * @author Rana Ahsan Ansar
  */
 public class Student extends Person {
-    
-    Student(String name , int age , int roll_number ){
-        super(name,age);
+
+    int roll_number;
+    int total;
+    int percentage;
+    List<Mcqs> mcqArray = new ArrayList<>();
+    private int score;
+
+    Student(String name, int age, int roll_number) {
+        super(name, age);
+        score = 0;
+        total = 0;
+        percentage = 0;
         mode = false;
-    }
-    
-    
-    public void attempt_Q(){
-        List<Mcqs> msqArray = new ArrayList<Mcqs>();
+        this.roll_number = roll_number;
+
+//        getting Array of Mcqs from DataBase 
         Connection connection = null;
         Statement statement = null;
         ResultSet resultSet = null;
-        
+
         try {
             connection = CreatConnection.getConnection();
             statement = connection.createStatement();
             resultSet = statement.executeQuery("SELECT * FROM `questions`");
-            
-            while (resultSet.next()){
+
+            while (resultSet.next()) {
                 Mcqs data = new Mcqs();
                 data.setId(resultSet.getInt("id"));
                 data.setQuestion(resultSet.getString("question"));
-                
+
                 data.setOpt1(resultSet.getString("opt1"));
                 data.setOpt2(resultSet.getString("opt2"));
                 data.setOpt3(resultSet.getString("opt3"));
                 data.setOpt4(resultSet.getString("opt4"));
-                
+
                 data.setCorrect(resultSet.getInt("correct"));
-                
-                msqArray.add(data);
-                
+
+                mcqArray.add(data);
+
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e);
-            
+
             e.printStackTrace();
         }
-        
-        System.out.println("");
-        
+
+    }
+
+    private void insertStudent() {
+        Connection connection = null;
         
     }
-    
+
+    public void attempt_Q() {
+//        Put Clear Screen Syntax Here Later
+
+        int answer = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Quiz Starts Now");
+        System.out.println("Enter 0 to Next\n\n");
+
+        for (Mcqs Q : mcqArray) {
+            total++;
+            System.out.println("Question No." + Q.getId() + ". " + Q.getQuestion() + "\n");
+
+            System.out.println("1. " + Q.getOpt1());
+            System.out.println("2. " + Q.getOpt2());
+            System.out.println("3. " + Q.getOpt3());
+            System.out.println("4. " + Q.getOpt4());
+            System.out.print("Enter your answer by 1, 2, 3, 4 according to Options given: ");
+
+            try {
+                answer = input.nextInt();
+                if (answer == Q.getCorrect()) {
+                    score++;
+                } else if (answer < 5 && answer > 0) {
+                    System.out.println("Your answer is Saved");
+                } else if (answer > 4 || answer == 0) {
+                    System.out.println("Going to Next Question...");
+                }
+
+            } catch (Exception e) {
+                System.out.println("Error: " + e);
+            }
+
+//            -------------------------------------------------------------------
+        }
+
+        System.out.println("Total " + total);
+        System.out.println("Your current Score: " + score);
+        insertStudent();
+
+    }
+
 }
